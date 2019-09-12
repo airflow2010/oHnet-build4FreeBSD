d41413f76d7f8284b9484ea7c741557fd74f013b
airflow2010 <florian.pressler@gmail.com>
2019-09-12 18:31:41 +0200

    script adaptations

--- ohNet-prep.bash
+++ ohNet-prep.bash
@@ -1,37 +1,45 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
-INPUTPATH=/usr/local/share/ohNet-build4FreeBSD/minim-lib-zip/
+WORKPATH=/usr/local/share/ohNet-build4FreeBSD
+INPUTPATH=/usr/local/share/ohNet-build4FreeBSD/minim-lib-zip
 
-OHNV=$1
-#OHNO="`find /mnt/SSD/jails/services_2/usr/local/share/minimserver/libext/ -name "ohnet*.zip"`"
+OHNO="`find $INPUTPATH -name "ohnet*.zip"`"
+REGEX=".*\(linux-intel\)-(.*).1.zip"
+
+if [[ $OHNO =~ $REGEX ]]
+	then 
+		echo Inputfile: $OHNO
+		echo Version: ${BASH_REMATCH[1]} 
+		OHNV=${BASH_REMATCH[1]}
+fi
+
 
 if [ $OHNV ]; then
 	echo orig lib: $OHNO
 	echo compiling ohNet_$OHNV.tar.gz from github as replacement? 
 	read x 
-	/usr/local/bin/wget https://github.com/openhome/ohNet/archive/ohNet_$OHNV.tar.gz
-	tar -xvzf ohNet_$OHNV.tar.gz -C /mnt/SSD/jails/build_1/usr/local/share/
-#	rm -rf ./ohNet_$OHNV.tar.gz
-	unzip $OHNO -d /mnt/SSD/jails/build_1/usr/local/share/ohnet-zip/
+	/usr/local/bin/wget -P $WORKPATH https://github.com/openhome/ohNet/archive/ohNet_$OHNV.tar.gz
+	tar -xvzf $WORKPATH/ohNet_$OHNV.tar.gz -C $WORKPATH
+	read x
+	unzip $OHNO -d $WORKPATH/minim-lib-zip-extracted/
 
-	jexec build_1 /usr/local/share/ohnet-compile.sh $OHNV
+	exit
+
+	$WORKPATH/ohNet-compile.sh $OHNV
 
 	echo move libs to archive?
 	read x
 	cp -av $OHNO /mnt/SSD/jails/build_1/usr/local/share/ /mnt/SSD/jails/build_1/usr/local/share/ohnet-$OHNV-orig.zip
 	cp -av /mnt/SSD/jails/build_1/usr/local/share/ohnet.zip /mnt/SSD/jails/build_1/usr/local/share/ohnet-$OHNV-freebsd.zip
 
-	echo replace $OHNO with newly compiled lib?
-	read x
-	cp -av /mnt/SSD/jails/build_1/usr/local/share/ohnet-$OHNV-freebsd.zip $OHNO 
-
 	echo remove working-dirs?
 	read x
 	rm -rf /mnt/SSD/jails/build_1/usr/local/share/ohNet-ohNet_$OHNV/
 	rm -rf /mnt/SSD/jails/build_1/usr/local/share/ohnet-zip/
 	rm -rf /mnt/SSD/jails/build_1/usr/local/share/ohnet.zip
+	rm -rf $WORKPATH/*.zip
 else
-echo Bitte Versionsnummer von ohnet angeben, zB $OHNO 
+echo cannot compute - no input version detected 
 fi
 
 
